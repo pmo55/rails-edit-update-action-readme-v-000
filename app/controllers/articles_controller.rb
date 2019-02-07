768328c0ca8c180156ca4056492ac443fb023dc6
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-07 16:53:14 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,6 +23,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end 
   def update
+    raise.params.inspect
   end
   # add edit and update methods here
 end
