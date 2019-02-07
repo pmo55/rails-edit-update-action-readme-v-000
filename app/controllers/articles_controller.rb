8f8be8a252e9031c75bb54a21104ed7c0e65e292
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-02-07 17:00:20 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,10 +22,9 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end 
-  def update
-    @article = Article.find(params[:id])
-    @article.update(title: params[:title], description: params[:description])
-    redirect_to article_path(@article)
-  end
-  # add edit and update methods here
+ def update
+  @article = Article.find(params[:id])
+  @article.update(title: params[:title], description: params[:description])
+  redirect_to article_path(@article)
+end
 end
